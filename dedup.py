1626323d2d0871328716ef87629986e5866586e4
Adam Wolk <adam.wolk@koparo.com>
2014-04-21 15:56:09 +0200

    Add reporting

--- dedup.py
+++ dedup.py
@@ -5,9 +5,13 @@ import sys
 import stat
 import hashlib
 
-target = sys.argv[1]
-
-print 'Searching for duplicates in %s' % target
+# http://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size
+def sizeof_fmt(num):
+    for x in ['bytes','KB','MB','GB']:
+        if num < 1024.0 and num > -1024.0:
+            return "%6.1f%6s" % (num, x)
+        num /= 1024.0
+    return "%6.1f%6s" % (num, 'TB')
 
 class FileKeeper(object):
     """Maintains a list of known files"""
@@ -24,6 +28,16 @@ class FileKeeper(object):
     def get(self, target):
         return self._entries[target.checksum()]
 
+    def report(self):
+        wasting_space = filter(lambda e: e.wasted() > 0, self._entries.values())
+        return sorted(wasting_space, key=lambda e: -e.wasted())
+
+    def wasted(self):
+        wasted = 0
+        for entry in self._entries.values():
+            wasted += entry.wasted()
+        return wasted
+
 class FileEntry(object):
     """A seen file"""
     def __init__(self, root, target):
@@ -31,6 +45,7 @@ class FileEntry(object):
         self._size = os.stat(self._path).st_size
         content = open(self._path).read()
         self._csum = hashlib.md5(content).hexdigest()
+        self._duplicates = []
         #print self
 
     def checksum(self):
@@ -47,21 +62,42 @@ class FileEntry(object):
         him= open(fileEntry.path()).read()
         return me == him
 
+    def wasted(self):
+        return self._size * len(self._duplicates)
+
+    def duplicate(self, fileEntry):
+        self._duplicates.append(fileEntry.path())
+
     def __str__(self):
-        return "path=%s\nsize=%s\ncsum=%s\n\n" % (self._path, self._size, self._csum)
+        return "%s wasted by duplicates of %s:\n%s" % (sizeof_fmt(self.wasted()), self._path, '\n'.join(self._duplicates))
 
-keeper = FileKeeper()
+def dedup(target):
+    keeper = FileKeeper()
 
-for root, dirs, files in os.walk(target):
-    print root
-    for f in files:
-        target = FileEntry(root, f)
-        if not keeper.seen(target):
-            keeper.add(target)
-        else:
-            seen = keeper.get(target)
-            if seen.size() != target.size() \
-             or not seen.identical(target):
+    print 'Searching for duplicates in %s' % target
+    for root, dirs, files in os.walk(target):
+        for f in files:
+            target = FileEntry(root, f)
+            if not keeper.seen(target):
                 keeper.add(target)
             else:
-                print "%s is a duplicate of:\n%s" % (f, seen)
+                seen = keeper.get(target)
+                if seen.size() != target.size() \
+                 or not seen.identical(target):
+                    keeper.add(target)
+                else:
+                    seen.duplicate(target)
+    return keeper
+
+def report(keeper):
+    for entry in keeper.report():
+        print
+        print entry
+
+    print
+    print "%s wasted in total" % sizeof_fmt(keeper.wasted())
+
+if __name__ == '__main__':
+    target = sys.argv[1]
+    keeper = dedup(target)
+    report(keeper)
